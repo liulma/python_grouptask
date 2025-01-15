3a9f7f18a6a6b40ac4768e6c47178fcedda41332
liulma <linda.ulma@skillio.ai>
2025-01-15 14:32:10 +0200

    Added ProductGenerator

---
+++ main.py
@@ -0,0 +1,16 @@
+from users import UserDataGenerator
+from transactions import Transaction
+from products import ProductGenerator
+
+users = 20
+products = 50
+transactions = 100
+user_generator = UserDataGenerator(num_users=users) 
+transaction_generator = Transaction(transaction_amount=transactions, user_amount=users, product_amount=products)
+product_generator = ProductGenerator(num_products=products)
+product_data = product.generator.get_product_data()
+user_data = user_generator.get_user_data()
+
+print(user_data)
+print(transaction_generator.transactions)
+print(product_data)
