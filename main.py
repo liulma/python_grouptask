3a58c190cc31cc0acd04e721c25afdaed76ea451
Miska Savenius <miska.savenius@gmail.com>
2025-01-15 14:23:01 +0200

    Luotu transactions.py ja transaction generaattori

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from users import UserDataGenerator
-
-user_generator = UserDataGenerator(num_users=20) 
+from transactions import Transaction
+users = 20
+products = 50
+transactions = 100
+user_generator = UserDataGenerator(num_users=users) 
+transaction_generator = Transaction(transaction_amount=transactions, user_amount=users, product_amount=products)
 user_data = user_generator.get_user_data()
 
 print(user_data)
+print(transaction_generator.transactions)
